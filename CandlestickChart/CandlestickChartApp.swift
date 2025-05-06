d2b097f47d5baf40cba29e28002d3ec12abec263
Lulwah Almisfer <con-lalmasfer@pif.gov.sa>
2025-05-06 22:40:19 +0300

    Initial Commit

---
+++ CandlestickChart/CandlestickChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandlestickChartApp.swift
+//  CandlestickChart
+//
+//  Created by Lulwah almisfer on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CandlestickChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CandlestickChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CandlestickChart
+//
+//  Created by Lulwah almisfer on 06/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
